5ad22a50d8af315c1ca39148b94c3e8c5402fe3b
Harry <hjwp2@cantab.net>
2013-12-29 16:37:05 +0000

    post to urls.login. --ch14l050--

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -6,7 +6,7 @@ var initialize = function (navigator, user, token, urls) {
     navigator.id.watch({
         loggedInUser: user,
         onlogin: function () {
-            $.post();
+            $.post(urls.login);
         }
     });
 };
