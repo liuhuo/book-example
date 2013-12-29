ce3de4c260d88fde1d5dae7e438ba55c2dbd2d24
Harry <hjwp2@cantab.net>
2013-12-29 17:03:16 +0000

    do navigator logout on post failure. --ch14l056--

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -9,7 +9,7 @@ var initialize = function (navigator, user, token, urls) {
             $.post(
                 urls.login,
                 { assertion: assertion, csrfmiddlewaretoken: token }
-            );
+            ).fail(function () { navigator.id.logout(); });
         },
         onlogout: function () {}
     });
