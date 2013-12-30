d3ec0e27dac1c5fa05ad7e7a037873185ebf325e
Harry <hjwp2@cantab.net>
2013-12-30 16:58:21 +0000

    refactor tests with setup+teardown. new test for non if status!=ok. --ch15l021--

--- accounts/tests/test_authentication.py
+++ accounts/tests/test_authentication.py
@@ -1,27 +1,38 @@
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 from django.test import TestCase
 
 from accounts.authentication import (
     PERSONA_VERIFY_URL, DOMAIN, PersonaAuthenticationBackend
 )
 
+mock_post = Mock()
+@patch('accounts.authentication.requests.post', mock_post)
 class AuthenticateTest(TestCase):
 
-    @patch('accounts.authentication.requests.post')
-    def test_sends_assertion_to_mozilla_with_domain(self, mock_post):
-        backend = PersonaAuthenticationBackend()
-        backend.authenticate('an assertion')
+    def setUp(self):
+        self.backend = PersonaAuthenticationBackend()
+        self.mock_response = mock_post.return_value
+        self.mock_response.ok = True
+
+    def tearDown(self):
+        mock_post.reset_mock()
+
+
+    def test_sends_assertion_to_mozilla_with_domain(self):
+        self.backend.authenticate('an assertion')
         mock_post.assert_called_once_with(
             PERSONA_VERIFY_URL,
             data={'assertion': 'an assertion', 'audience': DOMAIN}
         )
 
 
-    @patch('accounts.authentication.requests.post')
-    def test_return_none_if_response_errors(self, mock_post):
-        mock_response = mock_post.return_value
-        mock_response.ok = False
-        backend = PersonaAuthenticationBackend()
+    def test_return_none_if_response_errors(self):
+        self.mock_response.ok = False
+        user = self.backend.authenticate('an assertion')
+        self.assertIsNone(user)
 
-        user = backend.authenticate('an assertion')
+
+    def test_returns_none_if_status_not_okay(self):
+        self.mock_response.json.return_value = {'status': 'not okay!'}
+        user = self.backend.authenticate('an assertion')
         self.assertIsNone(user)
