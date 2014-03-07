415b6f72973d66b95f59314b17a319d5e6325c35
Liu Huo <huoliu@gmail.com>
2014-03-07 21:41:59 +0800

    Add new migration for list item uniqueness constraint

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -59,5 +59,6 @@ def _update_static_files(source_folder):
 
 def _update_database(source_folder):
     run('cd %s && ../virtualenv/bin/python3 manage.py syncdb --noinput' % (
-        source_folder,
-    ))
+        source_folder,))
+    run('cd %s && ../virtualenv/bin/python3 manage.py migrate lists --fake 0001' % (source_folder,))
+    run('cd %s && ../virtualenv/bin/python3 manage.py migrate' % (source_folder,))
